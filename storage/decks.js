71fb766c700922dc307292d16be26cb4d170f0dd
Jaap-Jan Frans <jaapjan@gmail.com>
2018-11-17 21:09:07 +0100

    decks asyncstorage

--- storage/decks.js
+++ storage/decks.js
@@ -1,11 +1,23 @@
+import { AsyncStorage } from "react-native"
+
+const DECKS = 'DECKS';
+let decks = {};
+
 export function saveDeck(deck) {
 
+    decks[deck.id] = deck;
+    return AsyncStorage.setItem(DECKS, deck);
+
 }
 
-export function deleteDeck(deck) {
-
+export function deleteDeck(deckId) {
+    delete decks[deckId];
+    return AsyncStorage.setItem(DECKS, decks);
 }
 
-export function loadDecks() {
+export async function loadDecks() {
+    const decksFromStorage = await AsyncStorage.getItem(DECKS);
+    decks = decksFromStorage;
 
+    return decksFromStorage;
 }
