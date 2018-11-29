ded92f09ab9b07e8fc99f2177e8fb1cdfd49845e
Jaap-Jan Frans <jaapjan@gmail.com>
2018-11-29 15:17:02 +0100

    removed padder

--- components/quiz/QuizResult.js
+++ components/quiz/QuizResult.js
@@ -17,7 +17,7 @@ const QuizResult = ({ onReset, score, totalNrOfCards }) => {
     let percentage = ((score / totalNrOfCards) * 100).toFixed(1);
     percentage = percentage % 1 > 0 ? percentage : Math.round(percentage);
 
-    return <Fragment padder>
+    return <Fragment>
                 <Text style={styles.padding}>{`Your score is ${percentage}%`}</Text>
                 <Button style={styles.marginBottom} block onPress={() => onReset(true)}><Text> Restart Quiz </Text></Button>
                 <Button block onPress={() => onReset(false)}><Text> Back to Deck </Text></Button>
